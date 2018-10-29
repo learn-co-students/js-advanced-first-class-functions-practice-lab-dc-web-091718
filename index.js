f188c09533987430476d12044e4f168059574667
Jacqueline Chenault <adigitalnative@gmail.com>
2018-10-29 15:53:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const logDriverNames = function(drivers) {
+  return drivers.map( driver => console.log(driver.name) )
+}
+
+const logDriversByHometown = function(drivers, hometown) {
+  const matchingDrivers = drivers.filter( driver => driver.hometown === hometown);
+  return logDriverNames(matchingDrivers);
+}
+
+const driversByRevenue = function(drivers) {
+  return drivers.slice().sort(function(driverOne, driverTwo) {
+    return driverOne.revenue - driverTwo.revenue
+  });
+}
+
+const driversByName = function(drivers) {
+  return drivers.slice().sort(function(driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  })
+}
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function(total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+}
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+}
