d7513b51133e3f4518a92a0cf8b369c41f058d44
serpentz <absalomcherinet@gmail.com>
2018-10-30 08:34:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+const logDriverNames = function(collection){collection.forEach(element => console.log(element.name))}
+const logDriversByHometown = function(collection,hometownToMatch){collection.filter(element => {element.hometown == hometownToMatch ? console.log(element.name) : 0})}
+const driversByRevenue = function(collection){
+	arr = [...collection]
+	arr.sort((element , element2)=> element.revenue - element2.revenue)
+	return arr
+}
+
+const driversByName = function(collection){
+	arr = [...collection]
+	arr.sort((a , b) => a.name.localeCompare(b.name))
+	return arr
+}
+const totalRevenue = collection => collection.reduce((accumulator, currentValue) => accumulator + currentValue.revenue,0);
+
+const averageRevenue = drivers =>  totalRevenue(drivers) /drivers.length 
+	 
